665252b6d2bbe3df03a6c0fd46da727e27b2c44a
Passimpay <118345040+Passimpay@users.noreply.github.com>
2023-05-19 03:16:47 +0300

    Add files via upload

---
+++ shop-script v1.0/lib/config/plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'name' => 'Passimpay',
+    'description' => 'Простая и интуитивная платежная система для бизнеса',
+    'icon' => 'img/logo_en16.png',
+    'logo' => 'img/logo_en.png',
+    'vendor' => 'webasyst',
+    'version' => '1.0.1',
+    'type' => waPayment::TYPE_ONLINE,
+);
